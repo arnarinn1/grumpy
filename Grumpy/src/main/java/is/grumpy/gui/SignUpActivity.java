f81b0154a0133f685fadafe66530842e6887dad3
Arnar <arnarh91@gmail.com>
2014-02-01 18:46:03 +0000

    Signup Events and stuff

--- Grumpy/src/main/java/is/grumpy/gui/SignUpActivity.java
+++ Grumpy/src/main/java/is/grumpy/gui/SignUpActivity.java
@@ -2,6 +2,10 @@ package is.grumpy.gui;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import is.grumpy.R;
 
@@ -10,6 +14,12 @@ import is.grumpy.R;
  */
 public class SignUpActivity extends ActionBarActivity
 {
+    private Button mSignup;
+    private EditText mUsernameField;
+    private EditText mPasswordField;
+    private EditText mPasswordConfirmField;
+    private EditText mFullNameField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,6 +31,31 @@ public class SignUpActivity extends ActionBarActivity
 
     private void Initialize()
     {
-        
+        mSignup = (Button) findViewById(R.id.signupUserButton);
+        mUsernameField = (EditText) findViewById(R.id.signupUsername);
+        mPasswordField = (EditText) findViewById(R.id.signupPassword);
+        mPasswordConfirmField = (EditText) findViewById(R.id.signupConfirmPassword);
+        mFullNameField = (EditText) findViewById(R.id.signupFullName);
+
+        mSignup.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //Call Rest api
+            }
+        });
+
+        mUsernameField.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus)
+            {
+                if (!hasFocus)
+                {
+                    //Call Rest Api to check if username exists.
+                }
+            }
+        });
     }
 }
