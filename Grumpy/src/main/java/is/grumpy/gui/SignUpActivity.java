5094b7cf348f0c0211df0cee63c798bb7d568f1e
Arnar <arnarh91@gmail.com>
2014-02-03 14:05:04 +0000

    Added Validation For Signup

--- Grumpy/src/main/java/is/grumpy/gui/SignUpActivity.java
+++ Grumpy/src/main/java/is/grumpy/gui/SignUpActivity.java
@@ -1,7 +1,10 @@
 package is.grumpy.gui;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,7 +45,7 @@ public class SignUpActivity extends ActionBarActivity
             @Override
             public void onClick(View v)
             {
-                //Call Rest api
+                SignUpUser();
             }
         });
 
@@ -54,8 +57,85 @@ public class SignUpActivity extends ActionBarActivity
                 if (!hasFocus)
                 {
                     //Call Rest Api to check if username exists.
+                    new CheckCredentialsWorker(getApplicationContext()).execute();
                 }
             }
         });
+    }
+
+    private void SignUpUser()
+    {
+        if(ValidateInputFields())
+        {
+            //Call Rest Api and Create User
+            new SignUpUserWorker().execute();
+        }
+    }
+
+    private boolean ValidateInputFields()
+    {
+        String userName = mUsernameField.getText().toString();
+        String fullName = mFullNameField.getText().toString();
+        String password = mPasswordField.getText().toString();
+        String confirmedPassword = mPasswordConfirmField.getText().toString();
+
+        if ((password.length() < 6) || (confirmedPassword.length() < 6))
+        {
+            Toast.makeText(this, "Password Length Must Exceed 5 Characters", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (!password.equals(confirmedPassword))
+        {
+            Toast.makeText(this, "Passwords Must Match", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (userName.length() == 0 || fullName.length() == 0)
+        {
+            Toast.makeText(this, "Must Enter a Username and Full Name", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private class CheckCredentialsWorker extends AsyncTask<String, Void, Boolean>
+    {
+        private Context mContext;
+
+        public CheckCredentialsWorker(Context context)
+        {
+            this.mContext = context;
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params)
+        {
+            try
+            {
+                return CheckIfUserExists(params[0]);
+            }
+            catch (Exception ex)
+            {
+                Log.e(getClass().getName(), ex.getMessage());
+            }
+
+            return null;
+        }
+
+        public boolean CheckIfUserExists(String userName)
+        {
+            return false;
+        }
+    }
+
+    private class SignUpUserWorker extends AsyncTask<String, Void, Boolean>
+    {
+        @Override
+        protected Boolean doInBackground(String... params)
+        {
+            return null;
+        }
     }
 }
