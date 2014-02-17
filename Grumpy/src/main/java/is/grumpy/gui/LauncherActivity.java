5af210d4ae1e90d2d0f8a5561713353dae857c1c
Arnar <arnarh91@gmail.com>
2014-02-17 17:49:15 +0000

    Tidy things up

--- Grumpy/src/main/java/is/grumpy/gui/LauncherActivity.java
+++ Grumpy/src/main/java/is/grumpy/gui/LauncherActivity.java
@@ -1,14 +1,14 @@
 package is.grumpy.gui;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 
 import is.grumpy.R;
 
 /**
  * Created by Arnar on 17.2.2014.
  */
-public class LauncherActivity extends ActionBarActivity
+public class LauncherActivity extends Activity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
