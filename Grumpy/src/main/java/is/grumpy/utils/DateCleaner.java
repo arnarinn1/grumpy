87955b7263f108a2ff5968939ba2d3b721dbb45b
Arnar <arnarh91@gmail.com>
2014-02-22 21:44:45 +0000

    Add minutes ago to DateCleaner

--- Grumpy/src/main/java/is/grumpy/utils/DateCleaner.java
+++ Grumpy/src/main/java/is/grumpy/utils/DateCleaner.java
@@ -13,28 +13,40 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by thdg9_000 on 15.2.2014.
  */
-public class DateCleaner {
-
+public class DateCleaner
+{
     private final Date date;
     private Context context;
 
-    public DateCleaner(String date, Context context) throws ParseException {
+    public DateCleaner(String date, Context context) throws ParseException
+    {
         this.date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date);
         this.context = context;
     }
 
-    public String getRelativeDate() {
+    public String getRelativeDate()
+    {
         Calendar today = Calendar.getInstance();
         today.set(Calendar.HOUR_OF_DAY, 0);
         today.set(Calendar.MINUTE, 0);
         today.set(Calendar.SECOND, 0);
         Date start_of_today = today.getTime();
         long time = start_of_today.getTime() - date.getTime();
-        if (time<0) { // happened today
+
+        if (time<0)
+        { // happened today
             Date now = new Date();
             time = now.getTime() - date.getTime();
+
+            long minutesAgo = TimeUnit.MILLISECONDS.toMinutes(time);
+            //Happened less than an hour ago
+            if (minutesAgo < 60)
+                return String.format("%d minutes ago", minutesAgo);
+
             return hoursAgo(TimeUnit.MILLISECONDS.toHours(time));
-        } else {
+        }
+        else
+        {
             long days = TimeUnit.MILLISECONDS.toDays(time);
             String hour = new SimpleDateFormat("HH:mm").format(this.date);
             if (days < 1)
@@ -44,15 +56,18 @@ public class DateCleaner {
         }
     }
 
-    private String yesterdayAt(String hour) {
+    private String yesterdayAt(String hour)
+    {
         return String.format(context.getString(is.grumpy.R.string.yesterday_at), hour);
     }
 
-    private String daysAgo(long days) {
+    private String daysAgo(long days)
+    {
         return String.format(context.getString(is.grumpy.R.string.days_ago), days);
     }
 
-    private String hoursAgo(long hours) {
+    private String hoursAgo(long hours)
+    {
         if (hours<1)
             return String.format(context.getString(is.grumpy.R.string.less_than), hours);
         return String.format(context.getString(is.grumpy.R.string.hours_ago), hours);
