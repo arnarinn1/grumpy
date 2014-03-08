6d85af4727d499ee13b5342147a9f8a06e92d2d1
Arnar <arnarh91@gmail.com>
2014-03-08 22:35:23 +0000

    Change launcher pic

--- Grumpy/src/main/java/is/grumpy/gui/SignUpActivity.java
+++ Grumpy/src/main/java/is/grumpy/gui/SignUpActivity.java
@@ -184,4 +184,11 @@ public class SignUpActivity extends ActionBarActivity
 
         return true;
     }
+
+    @Override
+    public void finish()
+    {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
 }
