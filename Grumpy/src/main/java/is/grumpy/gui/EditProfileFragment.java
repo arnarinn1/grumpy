b4a59ee5a1735fe08950470ba77d99e22d65e7dc
Arnar <arnarh91@gmail.com>
2014-03-24 03:05:54 +0000

    Refactor editprofile fragment

--- Grumpy/src/main/java/is/grumpy/gui/EditProfileFragment.java
+++ Grumpy/src/main/java/is/grumpy/gui/EditProfileFragment.java
@@ -1,56 +1,37 @@
 package is.grumpy.gui;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.hardware.Camera;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-import java.io.File;
-import java.net.URI;
-
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import is.grumpy.R;
-import is.grumpy.contracts.PostUser;
 import is.grumpy.contracts.ServerResponse;
 import is.grumpy.contracts.UserData;
 import is.grumpy.gui.base.BaseFragment;
 import is.grumpy.gui.base.BaseNavigationDrawer;
-import is.grumpy.gui.dialogs.SignupSuccessFulDialog;
 import is.grumpy.rest.GrumpyService;
 import is.grumpy.rest.RetrofitUtil;
 import retrofit.Callback;
-import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 
 /**
  * Created by thdg9_000 on 17.2.2014.
  */
-public class EditProfileFragment extends BaseFragment {
-
-    private UserData profile;
-
-    EditText mFirstName;
-    EditText mLastName;
-    EditText mBirthday;
-    EditText mAbout;
-    EditText mEmail;
-    Spinner mSex;
+public class EditProfileFragment extends BaseFragment
+{
+    @InjectView(R.id.first_name) EditText mFirstName;
+    @InjectView(R.id.last_name) EditText mLastName;
+    @InjectView(R.id.birth_year) EditText mBirthday;
+    @InjectView(R.id.about) EditText mAbout;
+    @InjectView(R.id.email) EditText mEmail;
+    @InjectView(R.id.spinner) Spinner mSex;
 
     private GrumpyService grumpyApi;
 
@@ -71,26 +52,10 @@ public class EditProfileFragment extends BaseFragment {
     {
         super.onActivityCreated(savedInstanceState);
 
-        mFirstName = (EditText) getView().findViewById(R.id.first_name);
-        mLastName = (EditText) getView().findViewById(R.id.last_name);
-        mBirthday = (EditText) getView().findViewById(R.id.birth_year);
-        mAbout = (EditText) getView().findViewById(R.id.about);
-        mEmail = (EditText) getView().findViewById(R.id.email);
-        mSex = (Spinner) getView().findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.sex_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSex.setAdapter(adapter);
-
-        RequestInterceptor requestInterceptor = new RequestInterceptor() {
-            @Override
-            public void intercept(RequestInterceptor.RequestFacade request)
-            {
-                request.addHeader("Content-Type", "application/json");
-                //If Connection header is not absent Java will throw an IO Error
-                request.addHeader("Connection", "close");
-            }
-        };
 
         RestAdapter restAdapter = RetrofitUtil.RestAdapterPostInstance(getActivity());
         grumpyApi = restAdapter.create(GrumpyService.class);
@@ -113,9 +78,9 @@ public class EditProfileFragment extends BaseFragment {
         String birthday = mBirthday.getText().toString();
         String sex = mSex.getSelectedItem().toString();
         String about = mAbout.getText().toString();
-        String email = mEmail.toString().toString();
+        String email = mEmail.getText().toString();
 
-        profile = new UserData();
+        UserData profile = new UserData();
         profile.setFirstName(firstName.trim());
         profile.setLastName(lastName.trim());
         profile.setBirthday(birthday.trim());
@@ -131,9 +96,10 @@ public class EditProfileFragment extends BaseFragment {
         @Override
         public void success(ServerResponse response, retrofit.client.Response response2)
         {
+            //TODO: Return updated user info to update cache credentials
             if (response.getStatus())
             {
-                Toast.makeText(getActivity(), "Yayyy!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "Updated User Info!", Toast.LENGTH_SHORT).show();
             }
             else
                 Toast.makeText(getActivity(), "Whoops, I have no idea what heppened", Toast.LENGTH_SHORT).show();
@@ -149,7 +115,9 @@ public class EditProfileFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        ButterKnife.inject(this, rootView);
+        return rootView;
     }
 
 }
